a75bb3b81ef83809fdc5c15c9d92a417ca89e773
algebraic-acrobat-1174 <mayowaa4@gmail.com>
2020-07-15 16:06:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,25 @@ function takeANumber(line, person){
   line.push(person)
   return `Welcome, ${person}. You are number ${line.length +1} in line.`
 }
+
+function nowServing(line){
+  if(line.length === 0){
+    return "There is nobody waiting to be served!"
+  }
+  else {
+    return line.shift()
+    
+  }
+}
+
+function currentLine(line){
+  if(line.length === 0){
+    return "The line is currently empty"
+  }
+  else{
+    var people = "The line is currently: "
+    for (var i = 0; i < line.length; i++) {
+      people = people + `${i+1}. ${line[i]}`
+    }
+  }
+}
